82e4d7365bab2c49ad930d0b8689de1b79ddf76b
MongiLearnsToCode <sandiledlmn@gmail.com>
2025-09-05 20:58:42 +0200

    fix: Correct authMiddleware import path

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,9 @@
-import { authMiddleware } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   publicRoutes: ["/", "/framework"],
 });
 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)']
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
